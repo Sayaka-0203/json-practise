c457bd3bab217a1bedb695fd930606e0ff19c34c
Sayaka-0203 <157017210+Sayaka-0203@users.noreply.github.com>
2024-06-22 21:54:12 +0900

    Update app.js

--- work/js/app.js
+++ work/js/app.js
@@ -3,11 +3,22 @@ document.addEventListener('DOMContentLoaded',function(){
   .then(response => response.json())
   .then(products => {
     const productTable = document.getElementById('productTable');
-    for(let i = 0;i < products.length; i++){
+    for(let i = 0; i < products.length; i++){
       const product = products[i];
-        const td = document.createElement('td');
-        td.textContent = `${product.id},${product.name},${product.price},${product.description}`;
-        productTable.appendChild(td);
+      const tr = document.createElement('tr');
+        const idTd = document.createElement("td");
+        idTd.textContent = product.id;
+        tr.appendChild(idTd);
+        const nameTd = document.createElement("td");
+        nameTd.textContent = product.name;
+        tr.appendChild(nameTd);
+        const priceTd = document.createElement("td");
+        priceTd.textContent = product.price;
+        tr.appendChild(priceTd);
+        const descriptionTd = document.createElement("td");
+        descriptionTd.textContent = product.description;
+        tr.appendChild(descriptionTd);
+        productTable.appendChild(tr);
     };
   })
   .catch(error => console.error('エラー:', error));
